704a9c3b6a4c12171bcffceeef9e5ef28ce1fbb3
Cedric Don Coloma <colomce@oocl.com>
2020-10-14 14:21:17 +0800

    [Collections][Cedric Coloma] refactor: Remove unnecessary NotImplementedException class and reformat code (alignment) in Filter.java.

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -1,15 +1,14 @@
 package com.thoughtworks.collection;
 
-import org.apache.commons.lang3.NotImplementedException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Filter {
 
-    List<Integer>  array;
+    List<Integer> array;
 
     public Filter(List<Integer> array) {
-     this.array = array;
+        this.array = array;
     }
 
     public List<Integer> filterEven() {
